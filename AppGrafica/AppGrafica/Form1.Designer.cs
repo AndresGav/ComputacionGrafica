d7dd6f6e3c0eb7889ce6a3ceeb08d7eda74da4d0
andresgavino11 <andresgavino11@hotmail.com>
2022-10-16 21:07:58 -0500

    All Project OK

--- AppGrafica/AppGrafica/Form1.Designer.cs
+++ AppGrafica/AppGrafica/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1003, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ENCENDER PIXEL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1003, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "DEGRADAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1003, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "PINTAR BANDERA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1003, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "BORRAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1145, 561);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ANDRES GAVINO";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
     }
 }
--- AppGrafica/AppGrafica/Form1.cs
+++ AppGrafica/AppGrafica/Form1.cs
@@ -2,9 +2,72 @@ namespace AppGrafica
 {
     public partial class Form1 : Form
     {
+        public static int width = 700, height = 500;
+        //bitmap
+        Bitmap bmp = new Bitmap(width, height);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmp.SetPixel(100, 100, Color.FromArgb(44, 62, 80));
+            bmp.SetPixel(100, 101, Color.FromArgb(44, 62, 80));
+            bmp.SetPixel(101, 100, Color.FromArgb(44, 62, 80));
+            bmp.SetPixel(101, 101, Color.FromArgb(44, 62, 80));
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 700; x++)
+            {
+
+                for (int y = 0; y < 500; y++)
+                {
+                    if (x < 350)
+                        bmp.SetPixel(x, y, Color.FromArgb(44, 62, 80));
+                    else
+                        bmp.SetPixel(x, y, Color.FromArgb(133, 146, 158));
+
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int red1 = 44, green1 = 62, blue1 = 80;
+            int red2 = 133, green2 = 146, blue2 = 158;
+            int red, green, blue;
+
+            for (int x = 0; x < 700; x++)
+            {
+                red = (int)lagrange(x, red1, red2);
+                green = (int)lagrange(x, green1, green2);
+                blue = (int)lagrange(x, blue1, blue2);
+
+                for (int y = 0; y < 500; y++)
+                {
+                    bmp.SetPixel(x, y, Color.FromArgb(red, green, blue));
+                }
+
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private int lagrange(int x, int inicial, int final)
+        {
+            return ((inicial * (x - 700)) / -700) + ((final * x) / 700);
         }
     }
 }
